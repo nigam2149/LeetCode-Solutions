b14bfdffdd7b3ec0a8ea902e806d181b2ae25321
Nigam Mishra <nigammishra214@gmail.com>
2026-01-26 19:56:17 +0530

    Add steps_to_zero solution

---
+++ Easy/Arrays/steps_to_zero.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int numberOfSteps(int num) {
+    int steps = 0;
+    while (num > 0) {
+        if (num % 2 == 0) num /= 2;
+        else num--;
+        steps++;
+    }
+    return steps;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";  // Prompt
+    cin >> num;
+    cout << "Steps: " << numberOfSteps(num) << endl;
+}
+
