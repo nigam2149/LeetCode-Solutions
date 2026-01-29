b5655a17f3d29c11896505d4482f0c80dd629281
Nigam Mishra <nigammishra2149@gmail.com>
2026-01-30 00:35:34 +0530

    Add: Add Two Numbers solution

--- Easy/LinkedList/addTwoNum.cpp
+++ Easy/LinkedList/addTwoNum.cpp
@@ -1,24 +1,40 @@
+// Problem: Add Two Numbers
+// Link: https://leetcode.com/problems/add-two-numbers/
+// Difficulty: Medium
+// Time Complexity: O(max(m, n))
+// Space Complexity: O(max(m, n))
+// Approach: Traverse both lists, handle carry
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode*dummy =new ListNode(0);
-        ListNode*current=dummy;
-        int carry=0;
-        while(l1 || l2 || carry){
-            int sum=carry;
-            if(l1){
-                sum+=l1->val;
-                l1=l1->next;
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+        
+        while(l1 || l2 || carry) {
+            int sum = carry;
+            
+            if(l1) {
+                sum += l1->val;
+                l1 = l1->next;
             }
-            if(l2){
-                sum+=l2->val;
-                l2=l2->next;
+            
+            if(l2) {
+                sum += l2->val;
+                l2 = l2->next;
             }
-            carry=sum/10;
-            int digit=sum%10;
-            dummy->next=new ListNode(digit);
-            dummy=dummy->next;           
+            
+            carry = sum / 10;
+            int digit = sum % 10;
+            
+            dummy->next = new ListNode(digit);
+            dummy = dummy->next;           
         }
+        
         return current->next;
     }
 };
